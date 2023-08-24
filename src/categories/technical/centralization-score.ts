c243feadc90334ee9124616d96d042514fbcc7d4
jguyet <gygydu19@hotmail.fr>
2023-08-25 00:10:22 +0200

    add empty descriptions

--- src/categories/technical/centralization-score.ts
+++ src/categories/technical/centralization-score.ts
@@ -2,35 +2,35 @@ import { runner } from "../../checker";
 
 const getContractUncertaintyCheck = (contract: any) => {
     const checkResult = runner(contract, [
-        { lbl: 'Hidden Source Code', fn: (c: any) => c.is_open_source != '1' },
-        { lbl: 'Proxy Contract', fn: (c: any) => c.is_proxy == '1' },
-        { lbl: 'External Call Risk', fn: (c: any) => c.external_call == '1' },
-        { lbl: 'Mint Function', fn: (c: any) => c.is_mintable == '1' },
-        { lbl: 'Self Destruct', fn: (c: any) => c.selfdestruct == '1' }
+        { lbl: 'Hidden Source Code', fn: (c: any) => c.is_open_source != '1', description: '' },
+        { lbl: 'Proxy Contract', fn: (c: any) => c.is_proxy == '1', description: '' },
+        { lbl: 'External Call Risk', fn: (c: any) => c.external_call == '1', description: '' },
+        { lbl: 'Mint Function', fn: (c: any) => c.is_mintable == '1', description: '' },
+        { lbl: 'Self Destruct', fn: (c: any) => c.selfdestruct == '1', description: '' }
     ], (_: any) => { return contract; });
     return checkResult;
 };
 
 const getContractOwnerPrivilegeCheck = (contract: any) => {
     const checkResult = runner(contract, [
-        { lbl: 'Retrieve Ownership', fn: (c: any) => c.can_take_back_ownership == '1' },
-        { lbl: 'Balance Modifiable', fn: (c: any) => c.owner_change_balance == '1' },
-        { lbl: 'Hidden Owner', fn: (c: any) => c.hidden_owner == '1' },
-        { lbl: 'Creator Percentage', fn: (c: any) => (c.creator_percent != undefined ? c.creator_percent*100 > 5 : false), getValue: (c: any) => `${(c.creator_percent != undefined ? c.creator_percent*100 : 0).toFixed(2)}%` },
-        { lbl: 'Owner Percentage', fn: (c: any) => (c.owner_percent != undefined ? c.owner_percent*100 > 5 : false), getValue: (c: any) => `${(c.owner_percent != undefined ? c.owner_percent*100 : 0).toFixed(2)}%` },
+        { lbl: 'Retrieve Ownership', fn: (c: any) => c.can_take_back_ownership == '1', description: '' },
+        { lbl: 'Balance Modifiable', fn: (c: any) => c.owner_change_balance == '1', description: '' },
+        { lbl: 'Hidden Owner', fn: (c: any) => c.hidden_owner == '1', description: '' },
+        { lbl: 'Creator Percentage', fn: (c: any) => (c.creator_percent != undefined ? c.creator_percent*100 > 5 : false), getValue: (c: any) => `${(c.creator_percent != undefined ? c.creator_percent*100 : 0).toFixed(2)}%`, description: '' },
+        { lbl: 'Owner Percentage', fn: (c: any) => (c.owner_percent != undefined ? c.owner_percent*100 > 5 : false), getValue: (c: any) => `${(c.owner_percent != undefined ? c.owner_percent*100 : 0).toFixed(2)}%`, description: '' },
     ], (_: any) => { return contract; });
     return checkResult;
 };
 
 const getContractTradingContraintCheck = (contract: any) => {
     const checkResult = runner(contract, [
-        { lbl: 'Honeypot', fn: (c: any) => c.is_honeypot == '1' },
-        { lbl: 'Buy Tax', fn: (c: any) => (c.buy_tax != undefined ? c.buy_tax*100 > 3 : false), getValue: (c: any) => `${(c.buy_tax != undefined ? c.buy_tax*100 : 0).toFixed(2)}%` },
-        { lbl: 'Sell Tax', fn: (c: any) => (c.sell_tax != undefined ? c.sell_tax*100 > 3 : false), getValue: (c: any) => `${(c.sell_tax != undefined ? c.sell_tax*100 : 0).toFixed(2)}%` },
-        { lbl: 'Modifiable Tax', fn: (c: any) => c.slippage_modifiable == '1' },
-        { lbl: 'Transfer Pausable', fn: (c: any) => c.transfer_pausable == '1' },
-        { lbl: 'Can Blacklist Wallet', fn: (c: any) => c.is_blacklisted == '1' },
-        { lbl: 'Can Whitelist Wallet', fn: (c: any) => c.is_whitelisted == '1' },
+        { lbl: 'Honeypot', fn: (c: any) => c.is_honeypot == '1', description: '' },
+        { lbl: 'Buy Tax', fn: (c: any) => (c.buy_tax != undefined ? c.buy_tax*100 > 3 : false), getValue: (c: any) => `${(c.buy_tax != undefined ? c.buy_tax*100 : 0).toFixed(2)}%`, description: '' },
+        { lbl: 'Sell Tax', fn: (c: any) => (c.sell_tax != undefined ? c.sell_tax*100 > 3 : false), getValue: (c: any) => `${(c.sell_tax != undefined ? c.sell_tax*100 : 0).toFixed(2)}%`, description: '' },
+        { lbl: 'Modifiable Tax', fn: (c: any) => c.slippage_modifiable == '1', description: '' },
+        { lbl: 'Transfer Pausable', fn: (c: any) => c.transfer_pausable == '1', description: '' },
+        { lbl: 'Can Blacklist Wallet', fn: (c: any) => c.is_blacklisted == '1', description: '' },
+        { lbl: 'Can Whitelist Wallet', fn: (c: any) => c.is_whitelisted == '1', description: '' },
     ], (_: any) => { return contract; });
     return checkResult;
 };
